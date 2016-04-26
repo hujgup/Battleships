517c88eed7a1ecdbaefd46f703bfcda4e7ebc83b
RubyDevlin <1005090954@student.swin.edu.au>
2016-04-27 00:38:56 +1000

    Add option for easy AI, ensured menu controller sets up AI correctly

--- src/GameController.cs
+++ src/GameController.cs
@@ -84,6 +84,9 @@ public static class GameController
 			case AIOption.Hard:
 				_ai = new AIHardPlayer(_theGame);
 				break;
+			case AIOption.Easy:
+				_ai = new AIEasyPlayer(_theGame);
+				break;
 			default:
 				_ai = new AIHardPlayer(_theGame);
 				break;
