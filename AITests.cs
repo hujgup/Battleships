3427d8afd98ba7c74866f0a81ef9b78562d54ea5
RubyDevlin <1005090954@student.swin.edu.au>
2016-05-08 15:06:37 +1000

    Created unit tests for AI, fixed score storage bug as merging the score storage fix branch didn't seem to work

---
+++ AITests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Battleships
+{
+	[TestFixture()]
+	public class BattleshipTests
+	{
+		[Test()]
+		public void TestAIExists ()
+		{
+			GameController.StartGame ();
+			Assert.IsNotNull (GameController.ComputerPlayer);
+
+		}
+	}
+}
+
